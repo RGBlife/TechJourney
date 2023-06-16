d985f19e723c307dfd9684b3d484439491f491f6
Bret <bret_gomes811@hotmail.co.uk>
2023-06-16 20:38:18 +0100

    Added Contact Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Layout } from "../components/Layout";
+import Contact from "../components/contact";
 
 const IndexPage = () => {
   return (
@@ -7,6 +8,7 @@ const IndexPage = () => {
       {/* Your page content goes here */}
       <h1>Hello, welcome to my portfolio!</h1>
       <p>This is a sample home page.</p>
+      <Contact />
     </Layout>
   );
 };
