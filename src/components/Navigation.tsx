aeeeadfba8f983d2df6b142566c877b5ba07ce08
Bret <bret_gomes811@hotmail.co.uk>
2023-06-17 14:56:09 +0100

    Removed Twitter Link

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
-import { Link } from "gatsby";
 
 export const Navigation = () => {
   return (
     <header className="p-4 bg-gray-800 text-white">
       <div className="flex items-center justify-between">
         <nav className="space-x-4 text-sm text-gray-200 flex">
-          {/* Use Gatsby's Link component for internal links and anchor for external links */}
-          <Link
-            to="/twitter"
-            className="hover:underline flex items-center space-x-1.5"
-          >
-            <span>Twitter</span>
-          </Link>
           <a 
             href="https://github.com/RGBlife?tab=repositories" 
             target="_blank" 
